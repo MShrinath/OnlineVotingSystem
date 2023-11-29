1cd395b6dd10a6ab19e47e87a563da8859eaf7ad
MShrinath <112850938+MShrinath@users.noreply.github.com>
2023-11-29 11:19:40 +0530

    added validation

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,56 +1,92 @@
 import { useState } from "react";
-import "../css/Register.css"
+import "../css/Register.css";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function Register() {
     const [first_name, setFirst_name] = useState("");
     const [last_name, setLast_name] = useState("");
-    const [dob, setDob] = useState(Date);
-    const [aadhaar_number, setAadhaar_number] = useState(null);
-    const [voter_id, setVoter_id] = useState(null);
+    const [dob, setDob] = useState("");
+    const [aadhaar_number, setAadhaar_number] = useState("");
+    const [voter_id, setVoter_id] = useState("");
     const [gender, setGender] = useState("");
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    // async function Register_User(event) {
-    //     event.preventDefault();
-
-    //     function over18(birthday) {
-    //         var optimizedBirthday = birthday.replace(/-/g, "/");
-    //         var myBirthday = new Date(optimizedBirthday);
-    //         var currentDate = new Date().toJSON().slice(0, 10) + ' 01:00:00';
-    //         var myAge = ((Date.now(currentDate) - myBirthday) / (31557600000));
-    //         if (myAge < 18)
-    //             return false;
-    //         else
-    //             return true;
-
-    //     }
-    //     if (over18(dob) === false) 
-    //         alert("You are under age")
-    //     console.log(gender)
-    // }
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        // Basic validation
+        if (
+            !first_name ||
+            !last_name ||
+            !dob ||
+            !aadhaar_number ||
+            !voter_id ||
+            !gender ||
+            !email ||
+            !username ||
+            !password
+        ) {
+            alert("All fields are required");
+            return;
+        }
+
+        // Age check
+        const currentDate = new Date();
+        const userDOB = new Date(dob);
+        const age = currentDate.getFullYear() - userDOB.getFullYear();
+        if (age < 18) {
+            alert("You must be at least 18 years old to register");
+            return;
+        }
+
+        // Username must be more than 3 characters
+        if (username.length < 3) {
+            alert("Username must be more than 3 characters");
+            return;
+        }
+
+        // Password validation (at least 6 characters for example)
+        if (password.length < 6) {
+            alert("Password must be at least 6 characters");
+            return;
+        }
+
+        // Aadhaar number should be exactly 12 numbers
+        if (!/^\d{12}$/.test(aadhaar_number)) {
+            alert("Aadhaar number should be exactly 12 numbers");
+            return;
+        }
+
+        // Voter ID should be exactly 10 numbers
+        if (!/^\d{10}$/.test(voter_id)) {
+            alert("Voter ID should be exactly 10 numbers");
+            return;
+        }
+
         try {
             await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/user/register`, {
-                first_name: first_name,
-                last_name: last_name,
-                dob: dob,
-                aadhaar_number:aadhaar_number,
-                voter_id:voter_id,
-                gender: gender,
-                email: email,
-                username: username,
-                password: password,
-                vote:"NONE",
-            })
+                first_name,
+                last_name,
+                dob,
+                aadhaar_number,
+                voter_id,
+                gender,
+                email,
+                username,
+                password,
+                vote: "NONE",
+            });
+            alert("Voter registered successfully!");
+            navigate('/vote');
         } catch (error) {
-            console.log("Error in Sending Data", error)
+            console.log("Error in Sending Data", error);
+            alert("Registration failed. Please try again.");
         }
-        alert("Voter registered successfully!")
     };
 
     return (
@@ -88,7 +124,7 @@ function Register() {
                     placeholder="Voter Id"
                 />
                 <select value={gender} onChange={(e) => setGender(e.target.value)}>
-                    <option defaultValue="" hidden>Select an Option</option> 
+                    <option defaultValue="" hidden>Select an Option</option>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
                     <option value="Other">Other</option>
