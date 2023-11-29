5ed8667dcecab7bc18b6a6cd35a1f488907046bc
MShrinath <112850938+MShrinath@users.noreply.github.com>
2023-11-29 12:21:30 +0530

    bg image + F404

--- client/src/pages/F404.js
+++ client/src/pages/F404.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "../css/F404.css";
 
 function F404() {
     return (
-        <div>
-            <a href="/"><h1>404</h1></a>
-            
+        <div className="error-container">
+            <center>
+                <h1>Page Not Found 404</h1>
+            </center>
         </div>
     );
 }
